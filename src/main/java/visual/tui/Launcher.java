8019854e6dff9f2b61863b478c6c537bcd9ad866
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-04-27 00:17:18 -0300

    remove Console class

--- src/main/java/visual/tui/Launcher.java
+++ src/main/java/visual/tui/Launcher.java
@@ -7,14 +7,17 @@ public class Launcher {
 
     public static void main(String[] args) {
 
-        if (System.getProperty("os.name").matches("^[Ww]in(down)?.*$")) {
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if (os.matches("win(downs)?.*")) {
 
             String java = String.format("\"%s%sbin%sjava.exe\" -cp \"%s%starget%sclasses\" visual.tui.Main",
                     System.getProperty("java.home"), File.separator, File.separator,
                     System.getProperty("user.dir"), File.separator, File.separator);
 
+            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "chcp 65001 > nul && " + java);
+
             try {
-                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "chcp 65001 > nul && " + java);
                 pb.inheritIO().start().waitFor();
             } catch (IOException | InterruptedException err) {
                 System.out.println("Failed to start");
