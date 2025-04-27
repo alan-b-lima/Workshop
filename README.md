26c61e1715166dc205c66ff9d927f5d81ebdaeea
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-04-27 00:07:02 -0300

    create Customer class; update README.md

--- README.md
+++ README.md
@@ -32,36 +32,35 @@ Esse é um Trabalho Prático desenvolvido como parte da disciplina de Programaç
 - visual\
     - gui\
     - tui\
-    - Functional.java
     - Strings.java
 - main\
     - auth\
-        - Account.java
-        - Authenticator.java
+        - [ ] Account.java
+        - [ ] Authenticator.java
     - financial\
-        - Expense.java
-        - Invoice.java
+        - [ ] Expense.java
+        - [ ] Invoice.java
     - common\
-        - Cpf.java
-        - Person.java
-        - Phone.java
-        - Vehicle.java
+        - [x] Cpf.java
+        - [x] Person.java
+        - [x] Phone.java
+        - [x] Vehicle.java
     - customer\
-        - Customer.java
+        - [x] Customer.java
     - exceptions\
     - workshop\
         - personnel\
-            - Employee.java
-            - Manager.java
-            - Shift.java
+            - [ ] Employee.java
+            - [x] Manager.java
+            - [x] Shift.java
         - service\
-            - Elevator.java
-            - Maintenance.java
-            - Part.java
-            - Scheduler.java
-            - Scheduling.java
-            - Service.java
-        - Workshop.java
+            - [ ] Elevator.java
+            - [ ] Maintenance.java
+            - [ ] Part.java
+            - [ ] Scheduler.java
+            - [ ] Scheduling.java
+            - [ ] Service.java
+        - [ ] Workshop.java
     
 ## Diagrama de Classes
 
---
+++ src/main/java/main/customer/Customer.java
@@ -0,0 +1,76 @@
+package main.customer;
+
+import json.JsonObject;
+import json.JsonSerializable;
+import main.common.Person;
+
+/**
+ * Classe que representa um cliente.
+ * 
+ * @author Alan Lima
+ */
+public class Customer extends Person implements JsonSerializable {
+
+    /**
+     * Endereço do cliente.
+     */
+    private String address;
+
+    /**
+     * Construtor padrão.
+     */
+    public Customer() {
+
+    }
+
+    /**
+     * Construtor parametrizado.
+     * 
+     * @param name    nome do cliente.
+     * @param phone   telefone do cliente.
+     * @param cpf     CPF do cliente.
+     * @param address endereço do cliente.
+     */
+    public Customer(String name, String phone, String cpf, String address) {
+        super(name, phone, cpf);
+        this.setAddress(address);
+    }
+
+    /**
+     * Retorna o endereço do cliente.
+     * 
+     * @return endereço do cliente.
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Define o endereço do cliente.
+     * 
+     * @param address endereço do cliente.
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toJson() {
+        return new JsonObject()
+                .add("name", this.getName())
+                .add("phone", this.getPhone())
+                .add("cpf", this.getFullCpf())
+                .add("address", this.getAddress())
+                .toJson();
+    }
+
+    /**
+     * Retorna uma representação textual do cliente.
+     * 
+     * @return representação textual do cliente.
+     */
+    @Override
+    public String toString() {
+        return String.format("{%s, %s, %s, %s}", this.getName(), this.getPhone(), this.getCpf(), this.getAddress());
+    }
+}
