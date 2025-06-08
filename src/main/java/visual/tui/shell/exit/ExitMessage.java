6a3aa47aed05fb64bb1f1799a67dfac3284d6d38
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-06-08 13:27:17 -0300

    implement exit behavior

--- src/main/java/visual/tui/shell/exit/ExitMessage.java
+++ src/main/java/visual/tui/shell/exit/ExitMessage.java
@@ -1,5 +1,14 @@
 package visual.tui.shell.exit;
 
-public class ExitMessage {
+/**
+ * Registro para mensagens de saída do shell.
+ * 
+ * @author Alan Lima
+ */
+public record ExitMessage(ExitCode code, String msg) {
     
+    /**
+     * Default exit message indicating a successful operation.
+     */
+    public static final ExitMessage DEFAULT = new ExitMessage(ExitCode.SUCCESS, null);
 }
