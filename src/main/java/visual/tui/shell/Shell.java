95b2b35fdcc1e04100c346ac9c4a7c3bee42f2f4
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-06-08 14:12:34 -0300

    comment Shell class

--- src/main/java/visual/tui/shell/Shell.java
+++ src/main/java/visual/tui/shell/Shell.java
@@ -3,18 +3,39 @@ package visual.tui.shell;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+/**
+ * Uma interface de linha de comando simples para executar comandos.
+ * 
+ * @author Alan Lima
+ */
 public class Shell {
-    
+
+    /**
+     * Contexto atual do shell, usado para exibir o prompt.
+     */
     private String context;
 
+    /**
+     * Construtor padrão do Shell, inicializa o contexto como uma string vazia.
+     */
     public Shell() {
         context = "";
     }
 
+    /**
+     * Padrão regex para remover espaços em branco à esquerda de uma string.
+     */
     private static final Pattern REMOVE_LEADING_WHITESPACE = Pattern.compile("\\s*(.*)");
 
+    /**
+     * Padrão regex para dividir uma string em palavras, usando espaços em branco
+     * como delimitadores.
+     */
     private static final Pattern SPLIT_PROMPT_PATTERN = Pattern.compile("\\s+");
 
+    /**
+     * Inicia o shell, permitindo que o usuário insira comandos.
+     */
     public void launch() {
         Scanner scanner = new Scanner(System.in);
 
@@ -38,11 +59,23 @@ public class Shell {
         scanner.close();
     }
 
+    /**
+     * Executa um comando com os argumentos fornecidos.
+     * 
+     * @param args argumentos do comando a ser executado.
+     */
     public static void runCommand(String... args) {
         Command command = Commands.getCommand(args[0]);
         command.execute(args);
     }
 
+    /**
+     * Decodifica o prompt removendo espaços em branco à esquerda e dividindo-o em
+     * palavras.
+     * 
+     * @param prompt a string do prompt a ser decodificada.
+     * @return um array de strings contendo as palavras do prompt.
+     */
     private static String[] decodePrompt(String prompt) {
         String trimmedPrompt = REMOVE_LEADING_WHITESPACE.matcher(prompt).replaceAll("$1");
         return SPLIT_PROMPT_PATTERN.split(trimmedPrompt);
